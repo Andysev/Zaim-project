cc45c842205be350bf44c4eb5297c7316d262965
Andysev <andy.golodyuk@gmail.com>
2021-05-06 19:52:21 +0300

    header, footer, first block, adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,45 @@
 const zaim = document.querySelector('.zaim');
 const credit = document.querySelector('.credit');
-
-const header = document.querySelector('.header_inner-tree')
+const mobZaim = document.querySelector('.mobZaim');
+const mobCredit = document.querySelector('.mobCredit');
+const menuMob = document.querySelector('.header_menu-mob');
+const headerMenu = document.querySelector('.header_mobile');
+const header = document.querySelector('.header_inner-tree');
+const mobDropZaim = document.querySelector('.mobDropZaim');
+const mobDropCredit = document.querySelector('.mobDropCredit');
+const arrowSvgZaim = document.querySelector('#zaimArrow');
+const arrowSvgCredit = document.querySelector('#creditArrow');
 
 zaim.addEventListener('mouseover', (event) => {
     const target = event.target
-    console.log(event.target)
     header.classList.toggle('marginTop')
 });
 zaim.addEventListener('mouseout', (event) => {
     const target = event.target
-    console.log(event.target)
     header.classList.toggle('marginTop')
 });
 credit.addEventListener('mouseover', (event) => {
     const target = event.target
-    console.log(event.target)
     header.classList.toggle('marginTop')
 });
 credit.addEventListener('mouseout', (event) => {
     const target = event.target
-    console.log(event.target)
     header.classList.toggle('marginTop')
+});
+menuMob.addEventListener('click', (event) => {
+    const target = event.target
+    menuMob.classList.toggle('menuMobActive')
+    headerMenu.classList.toggle('header_mobile-active')
+});
+
+mobZaim.addEventListener('click', (event) => {
+    const target = event.target
+    mobDropZaim.classList.toggle('mobDropZaim-active')
+    arrowSvgZaim.style.transform = "rotate(0deg)";
+    
+});
+mobCredit.addEventListener('click', (event) => {
+    const target = event.target
+    mobDropCredit.classList.toggle('mobDropCredit-active')
+    arrowSvgCredit.style.transform = "rotate(0deg)"
 });
